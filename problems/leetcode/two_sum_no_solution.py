0df71e1f6640f44ca1583c214b6531a116c43782
Annette3125 <anetuks@gmail.com>
2026-02-12 20:53:41 +0200

    feat(problems): add two sum no solution

---
+++ problems/leetcode/two_sum_no_solution.py
@@ -0,0 +1,20 @@
+def two_sum(nums, target):
+    seen = {}
+
+    for i, x in enumerate(nums):
+        need = target - x
+        if need in seen:
+            return [seen[need], i]
+        seen[x] = i
+    return None
+
+
+print(two_sum([2,7,11,15], 9))  # Expected: [0,1]
+print(two_sum([3,3], 6))        # Expected: [0,1]
+print(two_sum([3,2,4], 6))      # Expected: [1,2]
+
+assert two_sum([2,7,11,15], 9) == [0, 1]
+assert two_sum([3,3], 6) == [0, 1]
+assert two_sum([3,2,4], 6) == [1, 2]
+assert two_sum([1,2,3], 100) is None
+print("All tests passed.")
